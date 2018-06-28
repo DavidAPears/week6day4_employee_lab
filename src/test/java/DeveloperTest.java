90b6ed0d1f1c7ddb0cf0ea106c6bbc8a84a9331e
Iona Macbeth <ionalmacbeth@gmail.com>
2018-06-28 15:02:59 +0100

    payBonus for Developer tested OK

--- src/test/java/DeveloperTest.java
+++ src/test/java/DeveloperTest.java
@@ -1,2 +1,25 @@
+import org.junit.Before;
+import org.junit.Test;
+import techStaff.Developer;
+
+import static junit.framework.TestCase.assertEquals;
+
 public class DeveloperTest {
+
+    Developer developer;
+
+    @Before
+    public void before(){
+        developer = new Developer("Tony", "TS943435", 35000);
+    }
+
+    @Test
+    public void raiseSalary(){
+        assertEquals(36000, developer.raiseSalary(1000), 0.01);
+    }
+
+    @Test
+    public void payBonus(){
+        assertEquals(350, developer.payBonus(), 0.01);
+    }
 }
